03279440f7d5da989b34274241c168ae2a13072a
kz_morita <kz.morit@gmail.com>
2020-06-13 12:48:08 +0900

    Add test

--- src/lib.rs
+++ src/lib.rs
@@ -229,6 +229,15 @@ fn test_parse() {
             rhs: Box::new(Ast::Num(3.0))
         }
     );
+
+    assert_eq!(
+        parse(vec![Token::Operand(1.0), Token::Operator(Operator::Add)]),
+        Err(ParseError::MissingOperand)
+    );
+    assert_eq!(
+        parse(vec![Token::Operand(1.0), Token::Operand(2.0)]),
+        Err(ParseError::RemainingOperand)
+    );
 }
 
 #[test]
@@ -243,4 +252,14 @@ fn test_evaluate() {
         .evaluate(),
         3.0
     );
+}
+
+#[test]
+fn test_rpn() {
+    assert_eq!(rpn("123 + +"), Ok(6.0));
+    assert_eq!(rpn("1234+++"), Ok(10.0));
+    assert_eq!(rpn("83+5-9+"), Ok(15.0));
+    assert_eq!(rpn("893/*4+"), Ok(28.0));
+    assert!(rpn("9999+").is_err());
+    assert!(rpn("9++++").is_err());
 }
